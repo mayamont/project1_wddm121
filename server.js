96f0232b5fb1bb8bf7283f93e98b5c60c7af0077
joaopedroalencar <joaopedroalencar@hotmail.com>
2024-04-03 15:57:16 -0400

    files place changed

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(__dirname));
+app.use(express.static('assets'));
+
+app.use(express.static(__dirname + "/public"));
+app.set("views", "./views");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
+app.set("view engine", "ejs");
+
+
+//ROUTES HERE
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get("/", async function (req, res) {
+    res.render('fitnessTracker', {
+        title: 'Fitness Tracker',
+        page: 'fitnesstracker'
+    });
+});
+
+app.get("/profile", async function (req, res) {
+    res.render('profile', {
+        title: 'Profile',
+        page: 'profile'
+        // other data
+    });
+});
+
+app.get("/activities", async function (req, res) {
+    res.render('activities', {
+        title: 'Activities',
+        page: 'activities'
+    });
+});
+
+app.get("/workoutGraph", async function (req, res) {
+    res.render('workoutGraph', {
+        title: 'Workout Graph',
+        page: 'workout-graph'
+    });
+});
+
+//get sign in and sign up
+
+app.get("/signIn", async function (req, res) {
+    res.render('signin', {
+        title: 'Sign In',
+        page: 'signin'
+    });
+});
+
+app.get("/signUp", async function (req, res) {
+    res.render('signup', {
+        title: 'Sign Up',
+        page: 'signup'
+    });
+
+});
